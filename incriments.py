97cb2cd0291e398866cde0d64e2ec8f00d6af691
Rebecka Moreno <39251286+withaknotanh@users.noreply.github.com>
2022-04-12 13:07:03 -0400

    Add files via upload

---
+++ incriments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 12:03:59 2022
+
+@author: rebeckamoreno
+"""
+#Create a for loop to increment by .1 at each iteration 
+#Console outputs a numbe
+
+
+#rebecka version
+import numpy as np
+
+for x in np.arange(0, 1, 0.1):          #range is from 0 to 1 in 0.1 incriments
+    print(x)
+
+
+#more robust rebecka version?
+lower = int(input("enter min"))
+upper = int(input("enter max"))
+incriment = int(input("enter incriment"))  
+
+for x in np.arange(lower, upper, incriment):          
+    print(x)
