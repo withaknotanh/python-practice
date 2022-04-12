db4e7e4bb8b5e09f934c33a38e933e343e8286f7
Rebecka Moreno <39251286+withaknotanh@users.noreply.github.com>
2022-04-12 11:31:59 -0400

    py.py

---
+++ pi.py
@@ -0,0 +1,17 @@
+# Find PI to the Nth Digit 
+# Enter a number and have the program generate PI up to that many decimal places. 
+# Keep a limit to how far the program will go.
+
+#rebecka version 
+import math 
+import numpy 
+
+space = int(input("enter an integer less than 20"))
+
+if space < 20:
+    print(round(numpy.pi, space))
+
+else:
+    print("integer out of bounds")
+
+#hashmap version 
