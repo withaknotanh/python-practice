c30af4ece015d0277cdfd40eee6c98dca9da4de2
Rebecka Moreno <39251286+withaknotanh@users.noreply.github.com>
2022-04-12 14:07:18 -0400

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,13 +2,24 @@
 # Enter a number and have the program generate PI up to that many decimal places. 
 # Keep a limit to how far the program will go.
 
-#rebecka version 
-import numpy 
+#rebecka version where n is decimal places
+import numpy as np
 
-space = int(input("enter an integer less than 20"))                 #prompts user to enter integer 
+n = int(input("enter an integer less than 20"))                 #prompts user to enter integer 
 
-if space < 20:                                                      #if in bounds, round pi to the inputted integer amount
-    print(round(numpy.pi, space))
+if n < 20:                                                      #if in bounds, round pi to the inputted integer amount
+    print(np.round(np.pi, n))
+else:
+    print("integer out of bounds")  
+    
 
+#rebecka version where n is total places
+
+import numpy as np
+
+n = int(input("enter an integer less than 20"))                 #prompts user to enter integer 
+
+if n < 20:                                                      #if in bounds, round pi to the inputted integer amount
+    print(round(np.pi,(n-1)))
 else:
     print("integer out of bounds")  
