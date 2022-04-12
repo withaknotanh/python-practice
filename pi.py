4dad25ead3f9f59d86ae123e78804f0e87a8c381
Rebecka Moreno <39251286+withaknotanh@users.noreply.github.com>
2022-04-12 11:32:58 -0400

    Update pi.py

--- pi.py
+++ pi.py
@@ -3,13 +3,12 @@
 # Keep a limit to how far the program will go.
 
 #rebecka version 
-import math 
 import numpy 
 
-space = int(input("enter an integer less than 20"))
+space = int(input("enter an integer less than 20"))                 #prompts user to enter integer 
 
-if space < 20:
+if space < 20:                                                      #if in bounds, round pi to the inputted integer amount
     print(round(numpy.pi, space))
 
 else:
-    print("integer out of bounds")
+    print("integer out of bounds")  
