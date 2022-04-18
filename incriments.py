9403ca4da0ee5901380ccb84eeba40959cf34743
Rebecka Moreno <39251286+withaknotanh@users.noreply.github.com>
2022-04-18 11:32:55 -0400

    Update incriments.py

--- incriments.py
+++ incriments.py
@@ -14,7 +14,7 @@ import numpy as np
 for x in np.arange(0, 1, 0.1):          #range is from 0 to 1 in 0.1 incriments
     print(x)
 
-#more robust rebecka version?
+#more robust rebecka version? 
 import numpy as np
 
 lower = int(input("enter min"))
@@ -24,10 +24,9 @@ incriment = int(input("enter incriment"))
 for x in np.arange(lower, upper, incriment):          
     print(x)
 
-#more robust and numpy-less rebecka version?
-lower = int(input("enter min"))
-upper = int(input("enter max"))
-incriment = int(input("enter incriment"))  
-
-for x in range(lower,upper,incriment):          
-    print(x)
+#more robust and numpy-less rebecka version? #doesnt handle floats yet #want to make it user-input 
+def incrimentFun(start,end,incriment):
+    for x in range(start,end,incriment):
+        print(x)
+        
+incrimentFun(0,10,1)
